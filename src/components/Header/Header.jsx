d6891144296c45725ad19fb0fbbf3a82ff39a056
Aditta Chakraborty <adittacse@gmail.com>
2023-05-03 16:18:25 +0600

    navbar updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, {useContext, useState} from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import "./Header.css";
 import {AuthContext} from "../../providers/AuthProvider.jsx";
+import {Link} from "react-router-dom";
 
 const Header = () => {
     const {user} = useContext(AuthContext);
@@ -17,33 +18,38 @@ const Header = () => {
     
     return (
         <Navbar bg="dark" variant="dark" expand="md">
-            <div className="container ">
-                <Navbar.Brand href="#" className="flex-grow-1">Website Name</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" >
-                    <Nav className="ml-auto flex-grow-1">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/blog">Blog</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        {
-                            user ? (<NavDropdown
-                                    title={
-                                        <img src={user?.photoURL} alt="User Profile" className="profile-picture"/>
-                                    }
-                                    id="basic-nav-dropdown"
-                                    show={show}
-                                    onMouseEnter={showDropdown}
-                                    onMouseLeave={hideDropdown}
-                                >
-                                    <NavDropdown.Item disabled>{user?.displayName}</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
-                                </NavDropdown>)
-                                : (<Nav.Link href="/login">Login</Nav.Link>)
-                        }
-                    </Nav>
-                </Navbar.Collapse>
+            <div className="container" id="myNav">
+                <div>
+                    <Navbar.Brand href="#" className="">Website Name</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                </div>
+                <div>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto custom-nav">
+                            <Link className="nav-item" to="/">Home</Link>
+                            <Link className="nav-item" to="/blog">Blog</Link>
+                            {
+                                !user && <Link className="nav-item" to="/register">Register</Link>
+                            }
+                            {
+                                user ? (<NavDropdown
+                                        title={
+                                            <img src={user?.photoURL} alt="User Profile" className="profile-picture"/>
+                                        }
+                                        id="basic-nav-dropdown"
+                                        show={show}
+                                        onMouseEnter={showDropdown}
+                                        onMouseLeave={hideDropdown}
+                                    >
+                                        <NavDropdown.Item disabled>{user?.displayName}</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                                    </NavDropdown>)
+                                    : (<Nav.Link href="/login">Login</Nav.Link>)
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
             </div>
         </Navbar>
     );
