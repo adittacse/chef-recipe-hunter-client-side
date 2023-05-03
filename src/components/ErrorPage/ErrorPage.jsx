cd51b45028d8bae6f498ab3e7695b0bddd18e9b9
Aditta Chakraborty <adittacse@gmail.com>
2023-05-04 01:25:49 +0600

    404 page design updated

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 const ErrorPage = () => {
     const errorImage = "https://images.unsplash.com/photo-1578328819058-b69f3a3b0f6b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8NDA0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
@@ -7,7 +8,7 @@ const ErrorPage = () => {
         <Container className="my-5">
             <Row>
                 <Col md={6} className="text-center">
-                    <img src={errorImage} alt="404 Error" className="img-fluid" />
+                    <img style={{borderRadius: "10px"}} src={errorImage} alt="404 Error" className="img-fluid" />
                 </Col>
                 <Col md={6} className="d-flex align-items-center">
                     <div>
@@ -17,9 +18,7 @@ const ErrorPage = () => {
                             We're sorry, the page you requested could not be found. Please go
                             back to the homepage.
                         </p>
-                        <a href="/" className="btn btn-primary">
-                            Go to Homepage
-                        </a>
+                        <Link to="/" className="btn btn-dark">Go to Homepage</Link>
                     </div>
                 </Col>
             </Row>
