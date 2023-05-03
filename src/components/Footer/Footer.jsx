8fede099dd88936d7cf4431b67eab46f6284fdd4
Aditta Chakraborty <adittacse@gmail.com>
2023-05-03 17:02:00 +0600

    footer design updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import "./Footer.css";
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
     return (
         <div className="mt-5">
-            <footer className="bg-light py-3">
+            <footer className="bg-dark pt-5 pb-2 text-white">
                 <Container>
                     <Row>
                         <Col sm={6} md={3}>
-                            <h5>About Us</h5>
+                            <h5 className="mb-4">About Us</h5>
                             <p>We are a team of passionate chefs who love to share our knowledge and recipes with the world.</p>
                         </Col>
                         <Col sm={6} md={3}>
-                            <h5>Quick Links</h5>
+                            <h5 className="mb-4">Quick Links</h5>
                             <ul className="list-unstyled">
                                 <li><a href="#">Recipes</a></li>
                                 <li><a href="#">Videos</a></li>
@@ -21,7 +22,7 @@ const Footer = () => {
                             </ul>
                         </Col>
                         <Col sm={6} md={3}>
-                            <h5>Follow Us</h5>
+                            <h5 className="mb-4">Follow Us</h5>
                             <ul className="list-unstyled">
                                 <li><a href="#"><i className="fab fa-facebook fa-lg"></i> Facebook</a></li>
                                 <li><a href="#"><i className="fab fa-twitter fa-lg"></i> Twitter</a></li>
@@ -30,7 +31,7 @@ const Footer = () => {
                             </ul>
                         </Col>
                         <Col sm={6} md={3}>
-                            <h5>Contact Us</h5>
+                            <h5 className="mb-4">Contact Us</h5>
                             <p>
                                 123 Main St, Suite 456<br />
                                 Anytown, USA<br />
@@ -39,9 +40,10 @@ const Footer = () => {
                             </p>
                         </Col>
                     </Row>
-                    <Row className="mt-3">
+                    <Row className="mt-4">
                         <Col className="text-center">
                             <p>&copy; 2023 ChefSite. All rights reserved.</p>
+                            <p>Developed using react js</p>
                         </Col>
                     </Row>
                 </Container>
