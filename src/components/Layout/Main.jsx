7c4ff66e4c5de0ceb48370fc55e01f69654c30a4
Aditta Chakraborty <adittacse@gmail.com>
2023-05-03 11:28:42 +0600

    footer component added

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from "../Navigation/Navigation.jsx";
 import {Outlet} from "react-router-dom";
+import Footer from "../Footer/Footer.jsx";
 
 const Main = () => {
     return (
         <div>
             <Navigation></Navigation>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
