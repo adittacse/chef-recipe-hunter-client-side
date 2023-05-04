dcf14c2382dd01c5f8f85ec865ebf6ba816ccb9f
Aditta Chakraborty <adittacse@gmail.com>
2023-05-04 20:46:59 +0600

    navbar updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">BISTRO VOLTAIRE</Navbar.Brand>
+                <Navbar.Brand href="/">BISTRO VOLTAIRE</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto align-items-center align-content-md-start">
