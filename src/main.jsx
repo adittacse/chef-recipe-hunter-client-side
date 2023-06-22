a95b9e10da4ecb4a10ec3f330a154128e02fd68e
Aditta Chakraborty <adittacse@gmail.com>
2023-06-22 23:15:10 +0600

    server url updated

--- src/main.jsx
+++ src/main.jsx
@@ -21,12 +21,12 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>,
-                loader: () => fetch("https://chef-recipe-hunter-server-side-adittacse.vercel.app/chef/")
+                loader: () => fetch("https://chefrecipehunterserverside-adittacse.b4a.run/chef/")
             },
             {
                 path: "/chefdetails/:id",
                 element: <ChefDetails></ChefDetails>,
-                loader: ({params}) => fetch(`https://chef-recipe-hunter-server-side-adittacse.vercel.app/chef/${params.id}`)
+                loader: ({params}) => fetch(`https://chefrecipehunterserverside-adittacse.b4a.run/chef/${params.id}`)
             },
             {
                 path: "/blog",
